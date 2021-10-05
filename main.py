1c13eed81d9ae3afa6395dc5e9d0cb8f387a3c6e
chloedavis23 <818834@mystma.org>
2021-10-05 14:53:11 -0500

    2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #WRITE YOUR CODE IN THIS FILE
+def hasL(w): 
+
+
+    for x in range(0, len(w)): 
+        if w[x] == "l": 
+            return True
+
+    else: 
+        return False 
+
+print(hasL("alligator"))
